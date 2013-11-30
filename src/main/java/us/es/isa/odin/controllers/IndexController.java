95ff309ce86c4b9e742fadd244c8ec35e8334c55
Highfredo <highfredo@gmail.com>
2013-11-30 17:07:47 +0100

    Externalizada configuracion mejorado

--- src/main/java/us/es/isa/odin/controllers/IndexController.java
+++ src/main/java/us/es/isa/odin/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package us.es.isa.odin.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/welcome")
 public class IndexController {
 
+	@Value("${msg}")
+	private String msg;
+
 	@RequestMapping(value = "/index")
 	public ModelAndView index(@RequestParam(defaultValue = "hola") String q) {
 		ModelAndView modelAndView = new ModelAndView("index");
 		modelAndView.addObject("q", q);
+		modelAndView.addObject("msg", msg);
 		return modelAndView;
 	}
 
