45358b33305a3c8ae0e8aa38996c3dd31de2b2ae
Highfredo <highfredo@gmail.com>
2013-12-16 15:58:23 +0100

    Configuracion pasada a java config 3

---
+++ src/main/java/es/us/isa/odin/config/SpringConfig.java
@@ -0,0 +1,79 @@
+package es.us.isa.odin.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import com.mongodb.MongoClient;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"es.us.isa.odin"})
+public class SpringConfig extends WebMvcConfigurerAdapter {
+
+	// RESOURCES
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/favicon.ico").addResourceLocations("/favicon.ico");
+	}
+	//
+	 
+	// ServletHandling
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	//
+	
+	// DATA MONGO
+	@Bean
+	public MongoDbFactory mongoDbFactory() throws Exception {
+		UserCredentials credentials = new UserCredentials("odin", "odin");
+		return new SimpleMongoDbFactory(new MongoClient("ds057538.mongolab.com", 57538), "odindb", credentials);
+	}
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+		return mongoTemplate;
+	}
+	//
+	
+	// THYMELEAF
+	@Bean
+	public ServletContextTemplateResolver thymeleafTemplateResolver() {
+		ServletContextTemplateResolver context = new ServletContextTemplateResolver();
+		context.setPrefix("/views/");
+		context.setSuffix(".html");
+		context.setTemplateMode("HTML5");
+		context.setOrder(1);
+		context.setCacheable(false);
+		return context;
+	}
+	@Bean
+	public SpringTemplateEngine thymeleafTemplateEngine() {
+		//Set<IDialect> dialects = Sets.newHashSet(new Springse);
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+		//templateEngine.setAdditionalDialects(dialects);
+		return templateEngine;
+	}
+	@Bean
+	public ThymeleafViewResolver thymeleafViewResolver() {
+		ThymeleafViewResolver viewResolver =  new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(thymeleafTemplateEngine());
+		return viewResolver;
+	}
+	//
+	
+}
--- src/main/java/es/us/isa/odin/controllers/IndexController.java
+++ src/main/java/es/us/isa/odin/controllers/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import es.us.isa.odin.controllers.common.AbstractController;
@@ -18,32 +19,38 @@ import es.us.isa.odin.repositories.DocumentRepository;
 @RequestMapping("/welcome")
 public class IndexController extends AbstractController {
 
-	@Value("${msg}")
-	private String msg;
+	//@Value("${msg}")
+	//private String msg;
 	
-	@Autowired
-	private DocumentRepository repository;
+	//@Autowired
+	//private DocumentRepository repository;
 
 	@RequestMapping(value = "/index")
 	public ModelAndView index(@RequestParam(defaultValue = "hola mundo") String q) {
 		ModelAndView modelAndView = new ModelAndView("index");
 		modelAndView.addObject("q", q);
-		modelAndView.addObject("msg", msg);
+		//modelAndView.addObject("msg", msg);
 		return modelAndView;
 	}
 	
-	@RequestMapping(value = "/save")
-	public ModelAndView save(@RequestParam(defaultValue = "hola mundo") String q) {
-		ModelAndView modelAndView = new ModelAndView("index");
-		modelAndView.addObject("q", q);
-		modelAndView.addObject("msg", msg);
-		
-		FooEntity foo = new FooEntity();
-		foo.setBar(q);
-		Document<FooEntity> doc = new Document<>();
-		doc.setEntity(foo);
-		repository.save(doc);
-		return modelAndView;
+	@RequestMapping(value = "/index2")
+	@ResponseBody
+	public String index2(@RequestParam(defaultValue = "hola mundo") String q) {
+		return q;
 	}
+	
+//	@RequestMapping(value = "/save")
+//	public ModelAndView save(@RequestParam(defaultValue = "hola mundo") String q) {
+//		ModelAndView modelAndView = new ModelAndView("index");
+//		modelAndView.addObject("q", q);
+//		//modelAndView.addObject("msg", msg);
+//		
+//		FooEntity foo = new FooEntity();
+//		foo.setBar(q);
+//		Document<FooEntity> doc = new Document<>();
+//		doc.setEntity(foo);
+//		repository.save(doc);
+//		return modelAndView;
+//	}
 
 }
